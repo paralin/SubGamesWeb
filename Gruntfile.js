ff8796cf5e1e00c90f052bdc5b38170d83e1e632
Christian Stewart <kidovate@gmail.com>
2014-09-22 21:58:53 -0400

    Fix some production asset pipeline issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -362,6 +362,7 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
+            'assets/sounds/**/*',
             'index.html'
           ]
         }, {
